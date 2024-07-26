8e56a70dbb2c31e3f948db2ab781db344a725160
Bartosz1301 <bglowacki.kcl@gmail.com>
2024-07-26 17:38:03 +0100

    change2

---
+++ send_mail.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get form fields
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Your email address
+    $to = "bglowacki.kcl@gmail.com";
+
+    // Email subject
+    $subject = "New Contact Form Submission from $name";
+
+    // Email body
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Message sending failed.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
